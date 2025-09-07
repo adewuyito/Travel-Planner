2445b154e1444ed47948264c6f0296592ff6ad58
Timothy Adewuyi <adewuyitimothy10@gmail.com>
2025-09-07 21:58:18 +0100

    Initial Commit

---
+++ ItineraAi/ItineraAiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItineraAiApp.swift
+//  ItineraAi
+//
+//  Created by Gideon Adewuyi on 07/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct ItineraAiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
